f4de18e3ac83782ceaf8156f28993d14f5203607
ihaddou <ihaddou@btslivh.eu>
2019-01-10 15:17:43 +0100

    ajout du prjet vuemeteotesteur

---
+++ VueMeteotest/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: snir2g1
+ *
+ * Created on 20 décembre 2018, 18:17
+ */
+#include "VueMeteo.h"
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    VueMeteo maVueMeteo;
+    maVueMeteo.show();
+
+    return app.exec();
+}
