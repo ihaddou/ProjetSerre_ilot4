5b28d573bf890108ede0d87dc9e87b7e2804db3e
ccazenave <ccazenave@btslivh.eu>
2019-01-17 17:55:57 +0100

    ajout fichier info climat.h/.cpp vide

---
+++ ClassesMetier/infoclimat.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   infoclimat.cpp
+ * Author: snir2g1
+ * 
+ * Created on 15 janvier 2019, 18:26
+ */
+
+#include "infoclimat.h"
+
+infoclimat::infoclimat() {
+}
+
+infoclimat::infoclimat(const infoclimat& orig) {
+}
+
+infoclimat::~infoclimat() {
+}
+
---
+++ ClassesMetier/infoclimat.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   infoclimat.h
+ * Author: snir2g1
+ *
+ * Created on 15 janvier 2019, 18:26
+ */
+
+#ifndef INFOCLIMAT_H
+#define INFOCLIMAT_H
+
+class infoclimat {
+public:
+    infoclimat();
+    infoclimat(const infoclimat& orig);
+    virtual ~infoclimat();
+private:
+
+};
+
+#endif /* INFOCLIMAT_H */
+
